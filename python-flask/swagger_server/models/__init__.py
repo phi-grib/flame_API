bfa9878d1b71c21842c260ded039f02a607bfe96
BielStela <biel.stela@gmail.com>
2019-03-06 11:32:51 +0100

    cleaned old stuff

--- python-flask/swagger_server/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.model import Model
-from swagger_server.models.results import Results
