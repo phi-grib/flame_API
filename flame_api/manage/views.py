a8455e02ad200e19feaec38dddd1a9e5974afa53
BielStela <biel.stela@gmail.com>
2019-03-07 18:16:06 +0100

    fixi and skeletons

--- flame_api/manage/views.py
+++ flame_api/manage/views.py
@@ -1,3 +1,18 @@
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser, MultiPartParser
+from rest_framework import status
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from flame import manage
+from flame.util import utils
+
+class ModelsList(APIView):
+    """
+    Model list
+    """
+    def get(self, request):
+        models = manage.action_dir()  # fix what flame returns xd
+        return Response(models, 200)
