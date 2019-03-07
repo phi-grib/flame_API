e032581b05566a62879ed93b16ce5b1e9de536da
Biel Stela <biel.stela@gmail.com>
2019-03-07 16:25:56 +0100

    aded file handler in post build

--- flame_api/build/views.py
+++ flame_api/build/views.py
@@ -1,9 +1,27 @@
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser, MultiPartParser
+from rest_framework import status
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
-@api_view(['GET'])
-def build(request, modelname, version):
-    response = {"response": f"hey! I'm {modelname} version {version}"}
-    return JsonResponse(response)
+
+
+class Build(APIView):
+    """
+    Build model 
+    """
+    parser_classes = (MultiPartParser,)
+    def post(self, request, modelname, version, format=None):
+        file_obj = request.FILES['file']
+        for l in file_obj:
+            print(l)
+        response = {"received data": f"hey! I'm {modelname} version {version} with data {request.data}"}
+        return JsonResponse(response)
+
+# @api_view(['GET'])
+# def build(request, modelname, version):
+#     response = {"response": f"hey! I'm {modelname} version {version}"}
+#     return JsonResponse(response)
 # Create your views here.
