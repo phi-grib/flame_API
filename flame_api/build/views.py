f6f2f2fa9ebc0d3834fde7467e35d00541f7d4e5
Biel Stela <biel.stela@gmail.com>
2019-03-08 19:56:06 +0100

    updated Build endpoint

--- flame_api/build/views.py
+++ flame_api/build/views.py
@@ -1,11 +1,21 @@
+import tempfile
+import os
+
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.parsers import FileUploadParser, MultiPartParser
+from rest_framework.parsers import MultiPartParser, FileUploadParser
 from rest_framework import status
+
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.core.files.storage import FileSystemStorage
+from django.core.files.base import ContentFile
 
+from flame import build
+
+
+TEMP_DIR_ROOT = None  # Base root dir for temp, None -> /tmp
 
 
 class Build(APIView):
@@ -13,9 +23,30 @@ class Build(APIView):
     Build model 
     """
     parser_classes = (MultiPartParser,)
+
     def post(self, request, modelname, version, format=None):
-        file_obj = request.FILES['file']
-        for l in file_obj:
-            print(l)
-        response = {"received data": f"hey! I'm {modelname} version {version} with data {request.data}"}
+
+        # get the upladed file with name "file"
+        file_obj = request.FILES['fileName']
+
+        # Set the temp filesystem storage
+        temp_dir = tempfile.mkdtemp(prefix="train_set_", dir=TEMP_DIR_ROOT)
+        fs = FileSystemStorage(location=temp_dir)
+        # save the file to the new filesystem
+        path = fs.save(file_obj.name, ContentFile(file_obj.read()))
+
+        training_data = os.path.join(temp_dir, path)
+        # TODO: implement the actual flame build
+        builder = build.Build(modelname)
+        builder.run(training_data)
+        
+        response = {
+            "received data":
+            {
+                "path": os.path.join(temp_dir, path),
+                "model": modelname,
+                "version": version
+
+            }
+        }
         return JsonResponse(response, status=200)
