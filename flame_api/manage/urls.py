d34fae701b21e87ff5f055149cf199db4decc287
Biel Stela <biel.stela@gmail.com>
2019-03-11 18:17:51 +0100

    fixes

--- flame_api/manage/urls.py
+++ flame_api/manage/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import ListModels, CreateModel
+from .views import ListModels, ManageModel
 
 urlpatterns = [
     path('list', ListModels.as_view()),
-    path('create/modelname/<str:modelname>', CreateModel.as_view()),
+    path('/model/<str:modelname>', ManageModel.as_view()),
 ]
