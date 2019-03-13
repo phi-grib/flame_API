23adb1c5621965b23460dd4270d1bc59fbf66ce8
BielStela <biel.stela@gmail.com>
2019-03-13 14:07:45 +0100

    black code style

--- flame_api/manage/urls.py
+++ flame_api/manage/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ListModels, ManageModels, ManageVersions, TestUpload
 
 urlpatterns = [
-    path('models', ListModels.as_view()),
-    path('models/<str:modelname>', ManageModels.as_view()),
-    path('models/<str:modelname>/version/<str:version>', ManageVersions.as_view()),
-    path('test', TestUpload.as_view())
+    path("models", ListModels.as_view()),
+    path("models/<str:modelname>", ManageModels.as_view()),
+    path("models/<str:modelname>/version/<str:version>", ManageVersions.as_view()),
+    path("test", TestUpload.as_view()),
 ]
