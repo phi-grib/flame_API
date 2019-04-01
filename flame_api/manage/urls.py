ca923989b32bce9f523326476e8a79db2668aa3d
phi <phi@MacBook-Air-5.local>
2019-04-01 10:36:52 +0200

    Get info validation

--- flame_api/manage/urls.py
+++ flame_api/manage/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ListModels, ManageModels, ManageVersions, TestUpload, ManageParameters
+from .views import ListModels, ManageModels, ManageVersions, TestUpload, ManageParameters, ManageValidation
 
 urlpatterns = [
     path("models", ListModels.as_view()),
     path("models/<str:modelname>", ManageModels.as_view()),
     path("models/<str:modelname>/version/<int:version>", ManageVersions.as_view()),
     path("models/<str:modelname>/version/<int:version>/parameters",ManageParameters.as_view()),
+    path("models/<str:modelname>/version/<int:version>/validation",ManageValidation.as_view()),
     path("test", TestUpload.as_view()),
 ]
