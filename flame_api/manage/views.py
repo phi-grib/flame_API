1bd6e2b82ba1c1052991e4db098c3c3c869949f9
phi <phi@MacBook-Air-5.local>
2019-06-04 12:56:58 +0200

    images

--- flame_api/manage/views.py
+++ flame_api/manage/views.py
@@ -159,8 +159,7 @@ class ManageImport(APIView):
         # Exist Model
        
         if os.path.isdir(models_path+ '/' +model_name+'/'):
-            print('Exist')
-            return Response({'Model': "ERROR"},status=status.HTTP_200_OK)
+            return Response({'Model': model_name},status=status.HTTP_409_CONFLICT)
 
         fs = FileSystemStorage(location=models_path) #defaults to   MEDIA_ROOT  
         tarFile = fs.save(model_name+'.'+extension, file_obj)
