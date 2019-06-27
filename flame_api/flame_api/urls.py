1c39c9df297be8e8dbbfadc0d5b6ee0e0389b4f7
phi <phi@MacBook-Air-5.local>
2019-06-27 08:50:21 +0200

    API change

--- flame_api/flame_api/urls.py
+++ flame_api/flame_api/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     #url(r'^', include('API.urls')),
     url(r'^(?!/?static/)(?!/?media/)(?P<path>.*\..*)$', RedirectView.as_view(url='/static/%(path)s', permanent=False)),
     #path("admin/", admin.site.urls),
-    url("manage/", include("manage.urls")),
-    url("build/", include("build.urls")),
-    url("predict/", include("predict.urls")),
+    url("api/flame/manage/", include("manage.urls")),
+    url("api/flame/build/", include("build.urls")),
+    url("api/flame/predict/", include("predict.urls")),
 ]
