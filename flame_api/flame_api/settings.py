f9cbfbc133d2ef6cc2b0b05a567f5a4f6e80993e
IgnacioPasamontes <ignacio.pasamontes@upf.edu>
2019-06-28 14:11:07 +0200

    Chnage for docker

--- flame_api/flame_api/settings.py
+++ flame_api/flame_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "ul-c_)4d4pz!g42-$-(3a^v4--prqkfzgmxels2h1_!ul(pkj!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['tartaro', 'localhost', '127.0.0.1', '0.0.0.0']
 
