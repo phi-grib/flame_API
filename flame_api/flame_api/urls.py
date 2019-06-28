f4e644e01dc5ae6dda33353b8417780fc83560a7
IgnacioPasamontes <ignacio.pasamontes@upf.edu>
2019-06-28 15:47:31 +0200

    Docker change

--- flame_api/flame_api/urls.py
+++ flame_api/flame_api/urls.py
@@ -23,10 +23,7 @@ from django.contrib.staticfiles.views import serve
 urlpatterns = [
 
     url(r'^$', serve, kwargs={'path': 'index.html'}),
-    #url(r'^', include('API.urls')),
     url(r'^(?!/?static/)(?!/?media/)(?P<path>.*\..*)$', RedirectView.as_view(url='/static/%(path)s', permanent=False)),
-    #path("admin/", admin.site.urls),
-    # url("api/flame/api", include("manage.urls")),
     url("api/flame/manage/", include("manage.urls")),
     url("api/flame/build/", include("build.urls")),
     url("api/flame/predict/", include("predict.urls")),
