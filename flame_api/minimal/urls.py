63ffe8fbdf0a2d7e5aba1d98f02659f45cbd37cd
Ignacio Pasamontes <36951846+IgnacioPasamontes@users.noreply.github.com>
2019-08-13 17:15:59 +0200

    similarity API

--- flame_api/minimal/urls.py
+++ flame_api/minimal/urls.py
@@ -4,13 +4,8 @@ from .views import API, Ready, Alive
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # ...
-    # Route TemplateView to serve Swagger UI template.
-    #   * Provide `extra_context` with view name of `SchemaView`.
-    path('', TemplateView.as_view(
-        template_name='swagger-ui.html',
-        extra_context={'schema_url':'openapi-schema'}
-    ), name='swagger-ui'),
+   
+    path('api', API.as_view()),
     path("alive", Alive.as_view()),
     path("ready", Ready.as_view()),  
 ]
