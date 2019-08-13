9637d253346604b543a7e2f9f884520ee3fbc827
Ignacio Pasamontes <36951846+IgnacioPasamontes@users.noreply.github.com>
2019-08-13 17:15:59 +0200

    similarity API

--- flame_api/flame_api/settings.py
+++ flame_api/flame_api/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "drf_yasg",
     "corsheaders",
     "build.apps.BuildConfig",
     "manage.apps.ManageConfig",
