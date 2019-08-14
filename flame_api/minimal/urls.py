94b34faa4b949300b7ac2dbd492038e97ced1464
IgnacioPasamontes <ignacio.pasamontes@upf.edu>
2019-08-14 11:57:20 +0200

    FrontEnd

--- flame_api/minimal/urls.py
+++ flame_api/minimal/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from django.conf.urls import include
 from .views import API, Ready, Alive
 
 from django.views.generic import TemplateView
 
 urlpatterns = [
    
-    path('api', include('swagger_ui.urls')),
+    path('api', API.as_view()),
     path("alive", Alive.as_view()),
     path("ready", Ready.as_view()),  
 ]
