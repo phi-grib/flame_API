48fb66f7e9be78f2507843cc74262830c1275353
IgnacioPasamontes <ignacio.pasamontes@upf.edu>
2019-09-19 11:25:49 +0200

    API changes

--- flame_api/build/views.py
+++ flame_api/build/views.py
@@ -39,40 +39,28 @@ class BuildModel(APIView):
      
         params = request.POST.get('parameters')  
               
-        epd = utils.model_path(modelname, 0)
-        lfile = os.path.join(epd, 'training_series')
-
         # Set the temp filesystem storage
-        '''temp_dir = tempfile.mkdtemp(prefix="train_data_", dir=None)
-
+        temp_dir = tempfile.mkdtemp(prefix="train_data_", dir=None)
         fs = FileSystemStorage(location=temp_dir)
-        # save the file to the new filesystem
         path_SDF = fs.save(file_obj.name, ContentFile(file_obj.read()))
         training_data = os.path.join(temp_dir, path_SDF)
 
-        #Copy file
-        shutil.copy(training_data, lfile) '''
-        
+       
         # TODO: implement correctly flame build
-        command_build = {'endpoint': modelname, 'infile': lfile, 'param_string': params}
+        command_build = {'endpoint': modelname, 'infile': training_data, 'param_string': params}
 
-        #LOG.info(f'Starting building model {args.endpoint}'
-        #         f' with file {args.infile} and parameters {args.parameters}')
-        success, results = context.build_cmd(command_build, output_format='JSON')
-        #try:
-        #    success, results = context.build_cmd(command_build, output_format='JSON')    
-        #except Exception as e:
-        #    return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+        try:
+            success, results = context.build_cmd(command_build, output_format='JSON')
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)    
 
-        print (success)
-        print (results)
-        if (results[0]):
+        if success:
             if isinstance(file_obj, bool):
                 filename = "internal training set"
             else:
                 filename = file_obj.name
             response = {
-                "buildStatus": "Model builded succesfully",
+                "buildStatus": "Model build successfully",
                 "fileName":  filename,
                 "modelName": modelname,
                 "version": 0
@@ -85,4 +73,4 @@ class BuildModel(APIView):
       
         
 
-        
+           
