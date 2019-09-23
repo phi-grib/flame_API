158fcc960d71ffc5c839a17c2996b22b7e7688d0
Ignacio Pasamontes <36951846+IgnacioPasamontes@users.noreply.github.com>
2019-09-23 10:38:29 +0200

    update

--- flame_api/flame_api/urls.py
+++ flame_api/flame_api/urls.py
@@ -30,9 +30,9 @@ urlpatterns = [
    
     url(r'^$', serve,kwargs={'path': 'index.html'}), 
     #url(r'^$', views.HomePageView.as_view() ),
-    url(r'^(?!/?static/)(?!/?media/)(?P<path>.*\..*)$',
-        RedirectView.as_view(url='/static/%(path)s', permanent=False)),
-    url("api/v1/",include("minimal.urls")),
+    #url(r'^(?!/?static/)(?!/?media/)(?P<path>.*\..*)$',
+        #RedirectView.as_view(url='/static/%(path)s', permanent=False)),
+    url(r'[\w\.\d-]/api/v1/',include("minimal.urls")),
     url("api/v1/manage/", include("manage.urls")),
     url("api/v1/smanage/", include("smanage.urls")),
     url("api/v1/build/", include("build.urls")),
