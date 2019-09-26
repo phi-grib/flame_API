4aba963b2f6b2cf6ca55d1acf81e58d7f7bb717a
IgnacioPasamontes <ignacio.pasamontes@upf.edu>
2019-09-26 14:44:24 +0200

    api YAML

--- flame_api/minimal/views.py
+++ flame_api/minimal/views.py
@@ -5,10 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import JsonResponse
 import yaml
-
+from rest_framework_yaml.renderers import YAMLRenderer
 # Create your views here.
-
-
 class Ready(APIView):
     """
     Ready service
@@ -25,14 +23,17 @@ class Alive(APIView):
        
         return Response("I am alive", 200)
 
+
 class API(APIView):
     """
     Returns the API definition file
     """
+    renderer_classes = [YAMLRenderer]
     def get(self, request):
+        # Read YAML file
         with open("../flameAPI.yaml", 'r') as stream:
             data_loaded = yaml.safe_load(stream)
-        return Response(data_loaded, status=200, content_type="application/yaml")
+        return Response(data_loaded, status=200)
 
   
 
