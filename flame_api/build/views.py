2d80483c79dd4de26112172742bb501b75dc2869
IgnacioPasamontes <ignacio.pasamontes@upf.edu>
2019-10-14 15:49:52 +0200

    FrontEnd

--- flame_api/build/views.py
+++ flame_api/build/views.py
@@ -50,13 +50,13 @@ class BuildModel(APIView):
         # TODO: implement correctly flame build
         command_build = {'endpoint': modelname, 'infile': training_data, 'param_string': params}
         
-        try:
-            x = threading.Thread(target=buildThread, args=(command_build,'JSON'))
+        #try:
+        x = threading.Thread(target=buildThread, args=(command_build,'JSON'))
             #threads.append(x)
-            x.start()
+        x.start()
             #success, results = context.build_cmd(command_build, output_format='JSON')
-        except Exception as e:
-            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)    
+        #except Exception as e:
+        #return Response(str(e), status=status.HTTP_400_BAD_REQUEST)    
         '''if success:
             if isinstance(file_obj, bool):
                 filename = "internal training set"
