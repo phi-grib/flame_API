c9eef3cd05765f30a11f413ff23bb979960415fe
Ignacio Pasamontes <36951846+IgnacioPasamontes@users.noreply.github.com>
2019-11-15 09:56:34 +0100

    url

--- flame_api/flame_api/settings.py
+++ flame_api/flame_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "ul-c_)4d4pz!g42-$-(3a^v4--prqkfzgmxels2h1_!ul(pkj!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #ALLOWED_HOSTS = [ gethostname(), gethostbyname(gethostname()), ] 
 ALLOWED_HOSTS = ['*']
 
