c8c937d9a3d99c3d58db0c5b9fec0f9d0036a371
Ignacio Pasamontes <36951846+IgnacioPasamontes@users.noreply.github.com>
2020-01-29 19:04:50 +0100

    Change

--- flame_api/flame_api/settings.py
+++ flame_api/flame_api/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-#STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
+#STATIC_ROOT = '/static/'
 
 # CORS
 ANGULAR_APP_DIR = os.path.join(BASE_DIR, 'static')
