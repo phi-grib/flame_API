487110fd5b508d95c5a3acb64042411ccb3a7559
manuelpastor <manuel.pastor@upf.edu>
2020-05-28 14:21:11 +0200

    docker version (do not use in local!)

--- flame_api/static/main.js
+++ flame_api/static/main.js
@@ -9752,13 +9752,20 @@ var keycloakConfig = {
 var environment = {
     keycloakConfig: keycloakConfig,
     production: false,
-    baseUrl: 'http://localhost:8000/api/v1',
-    baseUrl_manage: 'http://localhost:8000/api/v1/manage/',
-    baseUrl_build: 'http://localhost:8000/api/v1/build/',
-    baseUrl_predict: 'http://localhost:8000/api/v1/predict/',
-    baseUrl_smanage: 'http://localhost:8000/api/v1/smanage/',
-    baseUrl_sbuild: 'http://localhost:8000/api/v1/sbuild/',
-    baseUrl_search: 'http://localhost:8000/api/v1/search/'
+    baseUrl: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1',
+    baseUrl_manage: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/manage/',
+    baseUrl_build: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/build/',
+    baseUrl_predict: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/predict/',
+    baseUrl_smanage: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/smanage/',
+    baseUrl_sbuild: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/sbuild/',
+    baseUrl_search: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/search/'
+    // baseUrl: 'http://localhost:8000/api/v1',
+    // baseUrl_manage: 'http://localhost:8000/api/v1/manage/',
+    // baseUrl_build: 'http://localhost:8000/api/v1/build/',
+    // baseUrl_predict: 'http://localhost:8000/api/v1/predict/',
+    // baseUrl_smanage: 'http://localhost:8000/api/v1/smanage/',
+    // baseUrl_sbuild: 'http://localhost:8000/api/v1/sbuild/',
+    // baseUrl_search: 'http://localhost:8000/api/v1/search/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
