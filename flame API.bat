f428329fd56a7c47020c6f7e906c9265bedbe0a1
Manuel Pastor <manuel.pastor@upf.edu>
2020-06-11 10:56:34 +0200

    code cleaning

--- flame API.bat
+++ flame API.bat
@@ -1,5 +1,5 @@
 @echo off
-@CALL "%userprofile%\Anaconda3\Library\bin\conda.bat" activate flame
+@CALL "%userprofile%\miniconda3\Library\bin\conda.bat" activate flame
 START /B python flame_api/manage.py runserver
 timeout /t 5 /nobreak > NUL
 "C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8000
