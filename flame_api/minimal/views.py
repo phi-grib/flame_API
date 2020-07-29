e75a9af574d05c49f0149aad786c59a217a90162
Manuel Pastor <manuel.pastor@upf.edu>
2020-07-29 08:12:34 +0200

    code cleaning and GPL v3 headers

--- flame_api/minimal/views.py
+++ flame_api/minimal/views.py
@@ -1,12 +1,35 @@
+#! -*- coding: utf-8 -*-
+
+# Description    Flame API minimum API for ToxHub integration
+#
+# Authors:       Manuel Pastor (manuel.pastor@upf.edu)
+#                Ignacio Pasamontes 
+#
+# Copyright 2018-2020 Manuel Pastor
+#
+# This file is part of Flame
+#
+# Flame is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation version 3.
+#
+# Flame is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Flame. If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import yaml
 from django.shortcuts import render
 from django.conf import settings
-import os
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from django.http import JsonResponse
-import yaml
+from rest_framework.response import Response
 from rest_framework_yaml.renderers import YAMLRenderer
-# Create your views here.
+from rest_framework.views import APIView
+
 class Ready(APIView):
     """
     Ready service
@@ -23,7 +46,6 @@ class Alive(APIView):
        
         return Response("I am alive", 200)
 
-
 class API(APIView):
     """
     Returns the API definition file
@@ -31,6 +53,7 @@ class API(APIView):
     renderer_classes = [YAMLRenderer]
     def get(self, request):
         # Read YAML file
+
         with open("../flameAPI.yaml", 'r') as stream:
             data_loaded = yaml.safe_load(stream)
         return Response(data_loaded, status=200)
