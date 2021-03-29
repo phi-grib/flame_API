b2aa3a0fb571dc837053db81fb0b6c4f5674dc81
Manuel Pastor <manuel.pastor@upf.edu>
2021-03-29 15:42:55 +0200

    support for SMARTS queries

--- flame_api/static/assets/jsme/init.js
+++ flame_api/static/assets/jsme/init.js
@@ -1,5 +1,6 @@
 function jsmeOnLoad() {
         jsmeApplet = new JSApplet.JSME("jsme_container", "615px", "400px");
+        // jsmeApplet = new JSApplet.JSME("jsme_container", "615px", "400px", {"options": "query"});
 }
 
 function clipmol(moltext) {
