5b50a434cfef75b9e540740079b1ccbb9ba7bdf6
Manuel Pastor <manuel.pastor@upf.edu>
2022-01-21 11:57:30 +0100

    new other tab

--- flame API.bat
+++ flame API.bat
@@ -2,4 +2,4 @@
 @CALL "%userprofile%\miniconda3\condabin\activate" flame
 START /B python flame_api/manage.py runserver 0.0.0.0:8000
 timeout /t 5 /nobreak > NUL
-"C:\Users\mpastor\AppData\Local\Mozilla Firefox\firefox.exe" http://localhost:8000
+"C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8000
