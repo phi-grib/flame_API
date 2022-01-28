87caee86d09c73ff439a37b2f1d72f9c38cc9305
manuel.pastor <manuel.pastor@upf.edu>
2022-01-28 16:27:13 +0100

    new error handlers

--- flame_api/manage/views.py
+++ flame_api/manage/views.py
@@ -300,7 +300,7 @@ class ManageVersions(APIView):
             if 'code' in result and result['code'] == 0:
                 return JsonResponse({'waiting': time.ctime(time.time())}, status=status.HTTP_200_OK)
             if 'code' in result and result['code'] == 1:
-                return JsonResponse({'aborted': result['error']}, status=status.HTTP_200_OK)
+                return JsonResponse({'aborted': result['message']}, status=status.HTTP_200_OK)
 
         
         return JsonResponse (result,status = status.HTTP_404_NOT_FOUND)
