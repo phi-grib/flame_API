9be077a37e951ba8216bab8ff9a05808062e0cc1
Manuel Pastor <manuel.pastor@upf.edu>
2022-10-19 19:47:36 +0200

    better version of flameAPI.bat for new versions of Windows11

--- flame APInew.bat
+++ flame APInew.bat
@@ -1,3 +1,4 @@
 @echo off
 @CALL "%userprofile%\miniconda3\condabin\activate" flame
-python flame_api/manage.py runserver 0.0.0.0:8000
+START "flame server" python flame_api/manage.py runserver 0.0.0.0:8000
+"C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8000
