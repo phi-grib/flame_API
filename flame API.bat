473efbec0d67a5b13c731619afa7870e368102db
Manuel Pastor <manuel.pastor@upf.edu>
2022-10-23 18:33:46 +0200

    update flame start

--- flame API.bat
+++ flame API.bat
@@ -1,5 +1,4 @@
 @echo off
 @CALL "%userprofile%\miniconda3\condabin\activate" flame
-START /B python flame_api/manage.py runserver 0.0.0.0:8000
-timeout /t 5 /nobreak > NUL
-"C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8000
+START "CTRL-C TO CLOSE!" python flame_api/manage.py runserver 0.0.0.0:8000
+START /REALTIME "" "http://localhost:8000"
--- flame APInew.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-@CALL "%userprofile%\miniconda3\condabin\activate" flame
-START "CLOSE USING CTRL-C!" python flame_api/manage.py runserver 0.0.0.0:8000
-"C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8000
