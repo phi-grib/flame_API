bd28a29a25f9a28bd31e1dfaffc56bf3efbadd74
manuelpastor <manuel.pastor@upf.edu>
2022-10-23 18:15:25 +0200

    new flameAPI

--- flame APInew.bat
+++ flame APInew.bat
@@ -1,4 +1,4 @@
 @echo off
 @CALL "%userprofile%\miniconda3\condabin\activate" flame
-START "flame server" python flame_api/manage.py runserver 0.0.0.0:8000
+START "CLOSE USING CTRL-C!" python flame_api/manage.py runserver 0.0.0.0:8000
 "C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8000
