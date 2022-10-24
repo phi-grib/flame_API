7e6dec473b47070aabad4424389168b59657b7fb
manuel.pastor <manuel.pastor@upf.edu>
2022-10-24 18:33:17 +0200

    stop python process

--- flame API.bat
+++ flame API.bat
@@ -1,4 +1,5 @@
 @echo off
 @CALL "%userprofile%\miniconda3\condabin\activate" flame
+REM "esto solo funcionaría en PowerShell" Get-Process "Python" | Stop-process
 START "CTRL-C TO CLOSE!" python flame_api/manage.py runserver 0.0.0.0:8000
 START /REALTIME "" "http://localhost:8000"
