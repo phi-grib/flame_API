b0fd9258e92cd30b609472b2e58fb5c97d71073e
Manuel Pastor <manuel.pastor@upf.edu>
2022-10-26 18:22:06 +0200

    GUI and bat updates

--- flame API.bat
+++ flame API.bat
@@ -1,5 +1,5 @@
 @echo off
 @CALL "%userprofile%\miniconda3\condabin\activate" flame
-REM "esto solo funcionaría en PowerShell" Get-Process "Python" | Stop-process
-START "CTRL-C TO CLOSE!" python flame_api/manage.py runserver 0.0.0.0:8000
+REM powershell.exe -c "Get-Process "Python" | Stop-process"
+START /REALTIME /MIN "flame server" python flame_api/manage.py runserver 0.0.0.0:8000
 START /REALTIME "" "http://localhost:8000"
