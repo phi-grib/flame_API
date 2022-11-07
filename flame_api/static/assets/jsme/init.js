f04595c5c62438b487bb060cd1828ec279a510ee
Manuel Pastor <manuel.pastor@upf.edu>
2022-11-07 15:19:38 +0100

    GUI improvements

--- flame_api/static/assets/jsme/init.js
+++ flame_api/static/assets/jsme/init.js
@@ -1,5 +1,5 @@
 function jsmeOnLoad() {
-        jsmeApplet = new JSApplet.JSME("jsme_container", "615px", "400px");
+        // jsmeApplet = new JSApplet.JSME("jsme_container", "615px", "400px");
         // jsmeApplet = new JSApplet.JSME("jsme_container", "615px", "400px", {"options": "query"});
 }
 
--- flame_api/static/assets/jsme/initSelector.js
+++ flame_api/static/assets/jsme/initSelector.js
@@ -1,8 +1,8 @@
 function jsmeOnLoad() {
-    jsmeApplet = new JSApplet.JSME("jsme_container_selector", "615px", "400px", {"options": "query"});
+    jsmeApplet2 = new JSApplet.JSME("jsme_container_selector", "615px", "400px", {"options": "query"});
 }
 
-function clipmol(moltext) {
+function clipmol2(moltext) {
     span = document.getElementById("molclipboard_selector");
     span.innerText=moltext
 }
